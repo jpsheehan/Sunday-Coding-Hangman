cd93ab47fc00b926357cf56fe4b88357c5b7b3bf
Jesse Sheehan <jesse@sheehan.nz>
2018-11-18 18:05:01 +1300

    Added some skeleton files.

---
+++ hangman.py
@@ -0,0 +1,21 @@
+'''A simple game of Hangman for one person.
+The original wordlists are from https://github.com/first20hours/google-10000-english'''
+
+
+def main():
+    '''The main function where we start the game.'''
+    pass
+
+
+def read_word_file(filename):
+    '''Reads a file containing words and returns a list of words.'''
+    pass
+
+
+def show_hanged_man(stage):
+    '''Prints the hanged man graphic onto the screen.'''
+    pass
+
+
+if __name__ == '__main__':
+    main()
